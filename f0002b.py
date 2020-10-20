f88da3465befafe758f1d03bede515a964dd380b
tigyiandras <71698671+tigyiandras@users.noreply.github.com>
2020-10-20 16:43:13 +0200

    Create f0002b.py

---
+++ f0002b.py
@@ -0,0 +1,11 @@
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
+print('a tyúk lesz kiírva az utolsó sorban')
